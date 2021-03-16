539b445e930dec888e038d4d2c52b8b61e2e32fa
roolie_glo <rookie123@qq.com>
2021-03-16 21:11:49 +0800

    Idea与git的集成

--- src/main/java/com/atguigu/springboot/Love.java
+++ src/main/java/com/atguigu/springboot/Love.java
@@ -2,6 +2,8 @@ package com.atguigu.springboot;
 
 public class Love {
     public static void main(String[] args) {
-        System.out.println("dev.....");
+        System.out.println("dev0.....");
+        System.out.println("master..update01");
     }
+
 }
