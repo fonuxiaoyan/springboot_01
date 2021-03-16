c6e42c7e6916148584a79cef14f7be9a01a1557a
roolie_glo <rookie123@qq.com>
2021-03-16 21:12:50 +0800

    dev update01

--- src/main/java/com/atguigu/springboot/Love.java
+++ src/main/java/com/atguigu/springboot/Love.java
@@ -3,5 +3,6 @@ package com.atguigu.springboot;
 public class Love {
     public static void main(String[] args) {
         System.out.println("dev.....");
+        System.out.println("dev....update01..");
     }
 }
